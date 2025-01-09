bf46d674ad0ccb5d5b5f041cd3dcc8408ff0f552
Hitesh_Gurnani <hiteshgurnani101@gmail.com>
2025-01-09 19:44:31 +0000

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
+      theme: ThemeData(),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
---
+++ test/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/home_page.dart';
+import 'package:helloworld/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
